150700f192660a12595b93a73593d97bcdd6e895
Daniel P. Clark <6ftdan@gmail.com>
2017-01-15 17:35:02 -0500

    dymanic setter plan

---
+++ lib/state_inspector/dynamic_setter.rb
@@ -0,0 +1,9 @@
+# TODO: This whole file
+#
+# 1) Create temp observer to grab method names
+# 2) Use all setter methods with current values
+# 3) For every method not reported to observer rename the method
+#    and rewrite the original with an observer to call the renamed method
+# 4) Restore observer list to original state.
+#
+# NOTE: Much of this is designed to work with side effect setters
