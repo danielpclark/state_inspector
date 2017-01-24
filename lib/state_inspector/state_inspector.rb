10ed5e1e40d2ec137f85e38f404a300c0a0cbcc2
Daniel P. Clark <6ftdan@gmail.com>
2017-01-23 19:45:43 -0500

    Close to manual setter implementation

--- lib/state_inspector/state_inspector.rb
+++ lib/state_inspector/state_inspector.rb
@@ -1,8 +1,50 @@
+require 'read_source'
+
 module StateInspector
   class StateInspector
-    def self.report who, what, old, new
-      key = who.respond_to?(:class_eval) ? who : who.class
-      Reporter[key].update who, what, old, new
+    def initialize base, **opts
+      @base = base # pass in self
+
+      ## setter_filter # Choose whether to enforce setters ending with equals
+      @setter_filter = opts.fetch(:setter_filter) { false }
+    end
+
+    def snoop_setters *setters
+      base.class_eval do
+        setters.
+          select {|m| @setter_filter ? m =~ /=\z/ : m }.
+          select {|m| ![:attr_writer, :attr_accessor].include?(instance_method(m).attr?) }.
+          each do |m|
+            original_method = instance_method(m)
+            define_method(m) do |*args, &block|
+              var = "@#{__method__.to_s.chop}"
+              tell_si var, instance_variable_get(var), *args, &block
+              original_method.bind(self).call(*args, &block)
+            end
+          end
+      end
+    end
+
+    def snoop_methods *meth0ds
+      base.class_eval do
+        meth0ds.
+          each do |m|
+            original_method = instance_method(m)
+            define_method(m) do |*args, &block|
+              tell_si __method__, *args, &block
+              original_method.bind(self).call(*args, &block)
+            end
+          end
+      end
+    end
+
+    private
+    def base
+      if @base.respond_to? :class_eval
+        @base
+      else
+        @base.class
+      end
     end
   end
 end
--- test/manual_setters_test.rb
+++ test/manual_setters_test.rb
@@ -14,6 +14,7 @@ class ManualSetterTest < Minitest::Test
   def observer; StateInspector::Reporter[M] end
   def test_adds_hook_to_setter_defined_manually
     m = M.new
+    m.state_inspector.snoop_setters :thing=
     m.thing = 42
     assert_empty observer.values
     # add hook
